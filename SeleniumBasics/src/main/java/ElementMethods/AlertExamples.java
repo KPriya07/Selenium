173047ad6fb7f5e9a70e5f8a6c5f09e953745c7d
Admin <Admin@192.168.1.203>
2023-04-10 16:12:38 +0100

    Added Alert msg program

---
+++ SeleniumBasics/src/main/java/ElementMethods/AlertExamples.java
@@ -0,0 +1,39 @@
+package ElementMethods;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class AlertExamples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");
+		ChromeOptions co=new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		WebDriver driver=new ChromeDriver(co);
+		driver.manage().window().maximize();
+		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+		WebElement button1=driver.findElement(By.xpath("//button[@onclick='jsAlert()']"));
+		button1.click();
+		driver.switchTo().alert().accept();
+		
+		WebElement button2=driver.findElement(By.xpath("//button[@onclick='jsConfirm()']"));
+		button2.click();
+		String AlertText=driver.switchTo().alert().getText();//to return alert text
+		driver.switchTo().alert().dismiss();//To cancel
+		WebElement button3=driver.findElement(By.xpath("//button[@onclick='jsPrompt()']"));
+		button3.click();
+		driver.switchTo().alert().sendKeys("Hello");
+		driver.switchTo().alert().accept();
+		
+		
+		
+
+	}
+
+}
